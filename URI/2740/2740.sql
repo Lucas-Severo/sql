9d249c86680e0c357b73d27e8bf0f6d54bcff15f
Lucas Rodrigues Severo <lucas_severo@outlook.com.br>
2020-06-06 11:32:43 -0400

    Adicionado exercício 2740 de sql do URI

---
+++ URI/2740/2740.sql
@@ -0,0 +1,12 @@
+(SELECT CONCAT('Podium: ', team) AS name
+    FROM league
+    LIMIT 3
+)
+
+UNION ALL
+
+(SELECT CONCAT('Demoted: ', team) AS name
+    FROM league
+    WHERE position IN 
+   (SELECT position FROM league ORDER BY position DESC LIMIT 2)
+);
